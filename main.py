dc52bb3eb6d4dc5aabc579bf2d837fc5265b01ea
firiox <faviogalaxygmail.com>
2025-11-13 16:51:53 -0400

    Agregamos nuevo sonido de error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import msvcrt
 import os
 import pygame
+import time
+from utilities import limpiar
 
 palabras = ["silla","dedo","teclado","bueno","killua","zanahoria"]
 jugando = True
@@ -15,31 +17,49 @@ os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.mixer.init()
 # Cargar el sonido
 sonido = pygame.mixer.Sound("sonido-boton.wav")
+error = pygame.mixer.Sound("error-beep.wav")
+# Antes del bucle principal
+point_sound = pygame.mixer.Channel(0)
+error_sound = pygame.mixer.Channel(1)
 while (jugando):
-    for palabra in palabras :        
-        for letra in palabra :
-            if (puntuacion < 10) :
-                string_puntuacion = "-" + str(puntuacion)
-            else :
-                string_puntuacion = str(puntuacion)
-            if (vidas > 0) :
-                print(tablero.format(string_puntuacion,str(vidas),letra))
-                tecla_presionada = msvcrt.getwch()
-                sonido.play()
-                print(tecla_presionada)
-                if (tecla_presionada == " ") :
+    for palabra in palabras :
+        if (jugando == False) :
+            break
+        else :
+            for letra in palabra :
+                if (puntuacion < 10) :
+                    string_puntuacion = "-" + str(puntuacion)
+                else :
+                    string_puntuacion = str(puntuacion)
+                if (vidas > 0) :
+                    limpiar()
+                    print(tablero.format(string_puntuacion,str(vidas),letra))
+                    tecla_presionada = msvcrt.getwch()
+                    
+                    print(tecla_presionada)
+                    if (tecla_presionada == " ") :
+                        jugando = False
+                        break
+                    elif (tecla_presionada == letra) :
+                        puntuacion = puntuacion + 1
+                        #Reproduce el sonido cuando acierta la tecla
+                        point_sound.play(sonido)                        
+                    else :
+                        vidas = vidas - 1
+                        #Reproduce el sonido cuando falla la tecla
+                        error_sound.play(error)
+                else :
+                    #Esperamos a que el sonido termine
+                    while error_sound.get_busy():
+                        time.sleep(0.05)
+                    print("You loose")
+                    print("Score:{}".format(str(puntuacion)))
                     jugando = False
                     break
-                elif (tecla_presionada == letra) :
-                    puntuacion = puntuacion + 1
-                else :
-                    vidas = vidas - 1
-            else :
-                print("You loose")
-                print("Score:{}".format(str(vidas)))
-                jugando = False
-                break
     if (jugando == True) :
+        #Esperamos a que el sonido termine
+        while point_sound.get_busy():
+            time.sleep(0.05)
         print("You win")
         print("Score:{}".format(str(puntuacion)))
         jugando = False
@@ -56,3 +76,4 @@ while (jugando):
 "X-------"
 "--------"
 """
+
